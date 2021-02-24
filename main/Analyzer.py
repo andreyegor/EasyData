ba62e68c0c946a92401320743978a15d9dce4e36
andreyegor <andreevegor10000@gmail.com>
2021-02-24 17:09:36 +0700

    Many little changes, create Analyzer docs

--- main/Analyzer.py
+++ main/Analyzer.py
@@ -3,16 +3,27 @@ class Analysis:
     keys_count = None
     good_lines = None
 
-    def __init__(self, text, pop_objects=[], auto_count=True):
+    def __init__(self, text, pop_keys=[''], auto_count=True):
+        """Сlass for get information from HeadCraken statistics
+
+        Args:
+            text (str): text from file
+            pop_keys (list, optional): keys that are not a button. Defaults to [''].
+            auto_count (bool, optional): Folse to manual work. Defaults to True.
+        """
+
         self.text = text
-        self.pop_objects = pop_objects
+        self.pop_keys = pop_keys
         self.GoodLines()
         if auto_count:
             self.CountKeys()
-            self.SymmetricSlice(self.keys_count)
-            self.PopObjects()
+            self.keys_count = self.PopKeys(self.keys_count)
+            self.keys_count = self.SymmetricSlice(self.keys_count)
 
     def CountKeys(self):
+        """This function counts button presses using the self.good lines array, 
+            you can only use this function after self.GoodLines()
+        """
         keys = {}
         old_line = []
         for line in self.good_lines:
@@ -26,33 +37,62 @@ class Analysis:
         return(out)
 
     def GoodLines(self):
+        """This function converts data of the format "{a, b, c}\n {a, b, c}"
+            into a two-dimensional list [[a, b, c], [a, b, c]]
+
+        Returns:
+            list: two-dimensional list [[a, b, c], [a, b, c]]
+        """
         out = []
         for line in self.text:
             line = line[1][1:-1].split(",")
             out.append(line)
+        out[0] = []
         self.good_lines = out
         return out
 
-    def PopObjects(self):
+    def PopKeys(self, in_dict):
+        """Pop keys from pop.pop_keys
+
+        Args:
+            in_dict (dict): Your dictionary
+
+        Returns:
+            [type]: Dictionary without keys from self.pop_keys
+        """
+        out = in_dict
         wrong_file_format_error = "Error: wrong file format"
-        out = self.keys_count
-        for pop_object in self.pop_objects:
+        for pop_object in self.pop_keys:
             out.pop(pop_object, wrong_file_format_error)
-        self.keys_count = out
         return out
 
-    def SymmetricSlice(self, in_dict, slice=1):
+    def SymmetricSlice(self, in_dict, slice_length=1):
+        """This function slise first and last parts of keys in dictionary by slice_length symbols.
+
+        Args:
+            in_dict (dict): Your dictionary
+            slice_length (int, optional): Quantity symbols. Defaults to 1.
+
+        Returns:
+            dict: Dictionary without first and last symbols.
+        """
         out = {}
         for key in in_dict:
-            out[key[slice:slice*-1]] = in_dict[key]
-        in_dict = out
+            out[key[slice_length:slice_length*-1]] = in_dict[key]
         return out
 
     def AddKey(self, key, keys):
+        """Create key in keys or increments the counter value of this key
+
+        Args:
+            key (str): Your key
+            keys (dict): Your dictionary
+        """
         try:
             keys[key] += 1
         except:
             keys[key] = 1
 
     def GetKeysCount(self):
+        """Return self.keys_count"""
         return self.keys_count
--- main/prewiew.py
+++ main/prewiew.py
@@ -2,15 +2,13 @@ import csv
 import Analyzer
 way_to_keyboard_key_file = r"main\test_files\key_example.csv"
 way_to_mouse_key_file = r"main\test_files\mkey_example.csv"
-keyboard_keys_pop = ('e', '')
-mouse_keys_pop = ('ouse_ke', '')
 
 keyboard_key_file = open(way_to_keyboard_key_file, newline='')
 mouse_key_file = open(way_to_mouse_key_file, newline='')
 
 analysed_keyboard = Analyzer.Analysis(
-    csv.reader(keyboard_key_file), keyboard_keys_pop)
-analysed_mouse = Analyzer.Analysis(csv.reader(mouse_key_file), mouse_keys_pop)
+    csv.reader(keyboard_key_file))
+analysed_mouse = Analyzer.Analysis(csv.reader(mouse_key_file))
 keyboard_key_file.close()
 mouse_key_file.close()
 print("Keyboard\n"+"-"*8)
